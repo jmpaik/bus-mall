6da1b1feeda243d76b5c43bffb48bf13022a21ad
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-10-04 14:22:48 -0700

    debugged

--- app.js
+++ app.js
@@ -31,103 +31,99 @@ new UserSurveyImages('USB', 'img/usb.gif');
 new UserSurveyImages('Water Can', 'img/water-can.jpg');
 new UserSurveyImages('Wine Glass', 'img/wine-glass.jpg');
 
-var tracker = {
+var image1 = document.getElementById('image1');
+var image2 = document.getElementById('image2');
+var image3 = document.getElementById('image3');
+var submitButton = document.getElementById('submit');
 
-  generateRandomNumber: function() {
-    return Math.floor(Math.random() * allImages.length);
-  },
 
-  images: document.getElementById('images'),
-
-  generateRandomImages: function() {
-    var image1 = document.getElementById('image1');
-    var image2 = document.getElementById('image2');
-    var image3 = document.getElementById('image3');
-    var number1 = this.generateRandomNumber();
-    var number2 = this.generateRandomNumber();
-    var number3 = this.generateRandomNumber();
-
-    image1.src = allImages[number1].filepath;
-    image2.src = allImages[number2].filepath;
-    image3.src = allImages[number3].filepath;
-    image1.name = allImages[number1].name;
-    image2.name = allImages[number2].name;
-    image3.name = allImages[number3].name;
-
-    while (image1.src === image2.src || image1.src === image3.src || image2.src === image3.src) {
-      this.generateRandomImages();
-    };
-    this.images.appendChild(image1);
-    this.images.appendChild(image2);
-    this.images.appendChild(image3);
-  },
-
-//Function to track clicks//
-  clickCounter: function(event) {
-    if (totalClicks < 26) {
-      totalClicks++;
-      for (var i = 0; i < allImages.length; i++) {
-        if (allImages[i].name === event.target.name) {
-          allImages[i].clicked++;
-        }
-      }
-      tracker.generateRandomImages;
-    }
-    else {
-      leftImage.removeEventListener;
-      centerImage.removeEventListener;
-      rightImage.removeEventListener;
-    }
-  },
-
-  generateEventListeners: function () {
-    leftImage = document.getElementById('image1');
-    centerImage = document.getElementById('image2');
-    rightImage = document.getElementById('image3');
-    submitButton = document.getElementById('submit');
-    leftImage.addEventListener('click', clickCounter);
-    centerImage.addEventListener('click', clickCounter);
-    rightImage.addEventListener('click', clickCounter);
-    submitButton.addEventListener('click', this.generateTable);
-  },
-
-//Function to make results table//
-  tableEl: document.getElementById('table'),
-
-  generateTableHeader: function() {
-    var trEl = document.createElement('tr');
-    var emptyThEl = document.createElement('th');
-
-    emptyThEl.textContent = '';
-    trEl.appendChild(emptyThEl);
-
-    for (var i in allImages) {
-      var thEl = document.createElement('th');
-      thEl.textContent = allImages[i].name;
-      trEl.appendChild(thEl);
-    }
-    this.tableEl.appendChild(trEl);
-  },
-
-  generateTableBody: function() {
-    var trEl = document.createElement('tr');
-    var totalsTdEl = document.createElement('td');
-    totalsTdEl.textContent = 'Number of Image Clicks';
-    trEl.appendChild(totalsTdEl);
-    for (var i in allImages) {
-      var tdEl = document.createElement('td');
-      tdEl.textContent = allImages[i].clicked;
-      trEl.appendChild(tdEl);
-    }
-    this.tableEl.appendChild(trEl);
-  },
-
-  generateTable: function() {
-    tracker.generateTableHeader();
-    tracker.generateTableBody();
-    submitButton.removeEventListener('click', generateTable);
-  },
+function generateRandomNumber() {
+  return Math.floor(Math.random() * allImages.length);
 };
 
-tracker.generateRandomImages();
-tracker.generateEventListeners();
+var images = document.getElementById('images');
+
+function generateRandomImages() {
+  var number1 = generateRandomNumber();
+  var number2 = generateRandomNumber();
+  var number3 = generateRandomNumber();
+
+  image1.src = allImages[number1].filepath;
+  image2.src = allImages[number2].filepath;
+  image3.src = allImages[number3].filepath;
+  image1.name = allImages[number1].name;
+  image2.name = allImages[number2].name;
+  image3.name = allImages[number3].name;
+
+  while (image1.src === image2.src || image1.src === image3.src || image2.src === image3.src) {
+    generateRandomImages();
+  };
+  images.appendChild(image1);
+  images.appendChild(image2);
+  images.appendChild(image3);
+}
+
+//Function to track clicks//
+function clickCounter(event) {
+  if (totalClicks < 26) {
+    totalClicks += 1;
+    for (var i = 0; i < allImages.length; i++) {
+      if (allImages[i].name === event.target.name) {
+        allImages[i].clicked += 1;
+      }
+    }
+    generateRandomImages();
+  }
+  else {
+    image1.removeEventListener;
+    image2.removeEventListener;
+    image3.removeEventListener;
+    submitButton.addEventListener('click', generateTable);
+  }
+}
+
+function generateEventListeners () {
+  image1.addEventListener('click', clickCounter);
+  image2.addEventListener('click', clickCounter);
+  image3.addEventListener('click', clickCounter);
+}
+
+//Function to make results table//
+var tableEl = document.getElementById('table');
+
+function generateTableHeader() {
+  var trEl = document.createElement('tr');
+  var emptyThEl = document.createElement('th');
+
+  emptyThEl.textContent = '';
+  trEl.appendChild(emptyThEl);
+
+  for (var i in allImages) {
+    var thEl = document.createElement('th');
+    thEl.textContent = allImages[i].name;
+    trEl.appendChild(thEl);
+  }
+  tableEl.appendChild(trEl);
+};
+
+function generateTableBody() {
+  var trEl = document.createElement('tr');
+  var totalsTdEl = document.createElement('td');
+  totalsTdEl.textContent = 'Number of Image Clicks';
+  trEl.appendChild(totalsTdEl);
+  for (var i in allImages) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allImages[i].clicked;
+    trEl.appendChild(tdEl);
+  }
+  tableEl.appendChild(trEl);
+}
+
+function generateTable() {
+  generateTableHeader();
+  generateTableBody();
+  submitButton.removeEventListener('click', generateTable);
+}
+
+generateRandomImages();
+generateEventListeners();
